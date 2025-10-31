c5057a0bc6fc644a2cf6c190e03950d691face15
imshixin <imsixn@163.com>
2025-10-31 10:48:04 +0800

    添加快捷开关代理的脚本

--- docs/scripts/zsh.sh
+++ docs/scripts/zsh.sh
@@ -107,6 +107,81 @@ zstyle ':autocomplete:*' delay 0.5  # seconds (float)
 setopt nonomatch
 EOF
 
+if ! grep -q "pon(){" ~/.zshrc;then
+printf  "\e[1;36m--------------------------------------------------\e[0m\n"
+printf  "\e[1;96m是否添加快捷开关代理的脚本？（y/n，默认为y）\e[0m"
+read -r is_install_proxy
+if [ "$is_install_proxy" != "n" -a "$is_install_proxy" != "N" ];then
+sudo -E apt install -y awk sed
+cat >> ~/.zshrc << EOF
+export PROXY_CONF="${HOME}/.proxyrc"
+pon(){
+  # 加载配置
+  # default_ip
+  # default_port
+  if [ -e "$PROXY_CONF" ];then
+    source $PROXY_CONF
+    printf "\e[36m直接回车使用默认代理 ${default_ip}:${default_port}
+或输入代理(格式如 \e[7m11\e[0;36m、\e[7m120.11\e[0;36m、\e[7m168.120.11\e[0;36m、\e[7m192.168.120.11\e[0;36m，可加上端口号:10808\e[0;36m)\e[0m\n:"
+  else
+    printf "\e[36m输入代理地址(格式如 \e[7m192.168.120.11:10808\e[0;36m)\e[0m\n:"
+  fi
+  read -r input
+  if [ -z "$input" ]; then
+    if ! echo "${default_ip}:${default_port}" | grep -Eq "^[0-9]{1,3}(\.[0-9]{1,3}){0,3}(:[0-9]{1,5})?$" ; then
+      printf "\e[36m无效的默认值，请检查文件${PROXY_CONF}: \e[7m${default_ip}:${default_port}\e[0m\n"
+      return 1
+    fi
+    #使用默认配置
+    export http_proxy="http://${default_ip}:${default_port}"
+    export https_proxy="http://${default_ip}:${default_port}"
+    printf "\e[36m已设置http/https代理地址: \e[7mhttp://${default_ip}:${default_port}\e[0m\n"
+  else
+    if ! echo $input | grep -Eq "^[0-9]{1,3}(\.[0-9]{1,3}){0,3}(:[0-9]{1,5})?$" ; then
+      printf "\e[36m不合规的输入：\e[7m$input\e[0m\n"
+      return 1
+    fi
+    #input=192.168.120.11:10808
+    if [ -z "${default_ip}" ];then
+      default_ip=192.168.120.11
+    fi
+    ip=$(echo "$input" | awk -F: '{print $1}') # 移除端口号
+    ip=$(echo "$ip" | sed -E -e "s/^[[:blank:]]*//" -e "s/[[:blank:]]*$//") # trim
+    echo "ip=${ip}"
+    port=$(echo "$input" | awk -F: '{print (NF>1?$2:"")}') # 获取端口号
+    echo "port=${port}"
+    dot_count=$(echo $ip | tr -cd "." | wc -m) # 获取ip中.的数量
+    case $dot_count in
+      0 ) ip=$(echo $default_ip | sed -E "s/(\.[0-9]+){1}$/\.${ip}/")
+      ;;
+      1 ) ip=$(echo $default_ip | sed -E "s/(\.[0-9]+){2}$/\.${ip}/")
+      ;;
+      2 ) ip=$(echo $default_ip | sed -E "s/(\.[0-9]+){3}$/\.${ip}/")
+      ;;
+      3 ) #skip
+      ;;
+    esac
+    if [ -z "${port}" ];then
+      port=${default_port}
+    fi
+    export http_proxy="http://${ip}:${port}"
+    export https_proxy="http://${ip}:${port}"
+    printf "\e[36m已设置http/https代理地址：\e[7mhttp://${ip}:${port}\e[0m\n"
+    #保存配置
+    echo "default_ip=${ip}\ndefault_port=${port}" > ${PROXY_CONF}
+  fi
+}
+
+pof(){
+  unset http_proxy https_proxy HTTP_PROXY HTTPS_PROXY
+  printf  "\e[36m已关闭代理\e[0m\n"
+}
+
+EOF
+fi
+printf  "\e[1;96m已添加开关代理函数，在终端使用命令'pon'开启代理，使用'pof'关闭代理 \e[0m\n"
+fi
+
 if ! grep -q "rosup(){" ~/.zshrc;then
 printf  "\e[1;36m--------------------------------------------------\e[0m\n"
 printf  "\e[1;96m是否添加ros2 humble的source脚本？（y/n，默认为y）\e[0m"
--- docs/zsh.md
+++ docs/zsh.md
@@ -189,6 +189,73 @@ rosup(){
 }
 ```
 
+快捷开启代理的脚本，放到`.zshrc`最后面：
+```bash
+export PROXY_CONF="${HOME}/.proxyrc"
+pon(){
+  # 加载配置
+  # default_ip
+  # default_port
+  if [ -e "$PROXY_CONF" ];then
+    source $PROXY_CONF
+    printf "\e[36m直接回车使用默认代理 ${default_ip}:${default_port}
+或输入代理(格式如 \e[7m11\e[0;36m、\e[7m120.11\e[0;36m、\e[7m168.120.11\e[0;36m、\e[7m192.168.120.11\e[0;36m，可加上端口号:10808\e[0;36m)\e[0m\n:"
+  else
+    printf "\e[36m输入代理地址(格式如 \e[7m192.168.120.11:10808\e[0;36m)\e[0m\n:"
+  fi
+  read -r input
+  if [ -z "$input" ]; then
+    if ! echo "${default_ip}:${default_port}" | grep -Eq "^[0-9]{1,3}(\.[0-9]{1,3}){0,3}(:[0-9]{1,5})?$" ; then
+      printf "\e[36m无效的默认值，请检查文件${PROXY_CONF}: \e[7m${default_ip}:${default_port}\e[0m\n"
+      return 1
+    fi
+    #使用默认配置
+    export http_proxy="http://${default_ip}:${default_port}"
+    export https_proxy="http://${default_ip}:${default_port}"
+    printf "\e[36m已设置http/https代理地址: \e[7mhttp://${default_ip}:${default_port}\e[0m\n"
+  else
+    if ! echo $input | grep -Eq "^[0-9]{1,3}(\.[0-9]{1,3}){0,3}(:[0-9]{1,5})?$" ; then
+      printf "\e[36m不合规的输入：\e[7m$input\e[0m\n"
+      return 1
+    fi
+    #input=192.168.120.11:10808
+    if [ -z "${default_ip}" ];then
+      default_ip=192.168.120.11
+    fi
+    ip=$(echo "$input" | awk -F: '{print $1}') # 移除端口号
+    ip=$(echo "$ip" | sed -E -e "s/^[[:blank:]]*//" -e "s/[[:blank:]]*$//") # trim
+    echo "ip=${ip}"
+    port=$(echo "$input" | awk -F: '{print (NF>1?$2:"")}') # 获取端口号
+    echo "port=${port}"
+    dot_count=$(echo $ip | tr -cd "." | wc -m) # 获取ip中.的数量
+    case $dot_count in
+      0 ) ip=$(echo $default_ip | sed -E "s/(\.[0-9]+){1}$/\.${ip}/")
+      ;;
+      1 ) ip=$(echo $default_ip | sed -E "s/(\.[0-9]+){2}$/\.${ip}/")
+      ;;
+      2 ) ip=$(echo $default_ip | sed -E "s/(\.[0-9]+){3}$/\.${ip}/")
+      ;;
+      3 ) #skip
+      ;;
+    esac
+    if [ -z "${port}" ];then
+      port=${default_port}
+    fi
+    export http_proxy="http://${ip}:${port}"
+    export https_proxy="http://${ip}:${port}"
+    printf "\e[36m已设置http/https代理地址：\e[7mhttp://${ip}:${port}\e[0m\n"
+    #保存配置
+    echo "default_ip=${ip}\ndefault_port=${port}" > ${PROXY_CONF}
+  fi
+}
+
+pof(){
+  unset http_proxy https_proxy HTTP_PROXY HTTPS_PROXY
+  printf  "\e[36m已关闭代理\e[0m\n"
+}
+
+```
+
 打开终端时自动激活ros环境的脚本，**暂不推荐使用**，使用powerlevel10k主题时有兼容问题
 ```sh
 # 自动source ros环境，目前不建议使用
